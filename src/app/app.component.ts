538ab7645da7447143b96b6a3860310eda2492c6
davidgalileo <davidmarcelinoms@alumnos.iesgalileo.es>
2023-03-23 22:16:50 +0100

    ~Refinada lógica de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,12 @@ export class AppComponent {
 
   login: string = "";
 
+  constructor(private router: Router){}
+
   ngOnInit(){
     if(localStorage.getItem("login") == null){
       this.login = "false";
+      this.router.navigate(['/registro']);
     }
   }
 }
