fe80ccb2f84609a635716c6da022492e7d108f16
davidgalileo <davidmarcelinoms@alumnos.iesgalileo.es>
2023-04-12 09:59:04 +0200

    ~Credenciales de la base de datos

--- src/app/php/db.php
+++ src/app/php/db.php
@@ -5,7 +5,7 @@
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ];
 
-    $conexion = new PDO("mysql:host=localhost;dbname=id20011369_multi21", "id20011369_root", "X3chv2!MO[[-g3te", $options);
+    $conexion = new PDO("mysql:host=localhost;dbname=id20011369_multi21", "id20011369_root", "ProyectoTFG2023@", $options);
     if($conexion->errorCode()){
         echo "Error en la conexion";
     }else{
