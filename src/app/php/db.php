428481e1f9f23da8ddc335cc2447b23aa5093e9e
davidgalileo <davidmarcelinoms@alumnos.iesgalileo.es>
2023-04-12 08:57:24 +0200

    +Cambio en las credenciales de la base de datos

--- src/app/php/db.php
+++ src/app/php/db.php
@@ -5,7 +5,7 @@
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ];
 
-    $conexion = new PDO("mysql:host=localhost;dbname=multi21", "root", "", $options);
+    $conexion = new PDO("mysql:host=localhost;dbname=id20011369_multi21", "id20011369_root", "X3chv2!MO[[-g3te", $options);
     if($conexion->errorCode()){
         echo "Error en la conexion";
     }else{
